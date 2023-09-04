7f848d749ffb28715184e1fb20a024162465e72f
nathanhtkd <nathanhartojo@gmail.com>
2023-09-04 22:53:18 +0000

    Finished TDD Development for real

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
     validates :title, presence: true
     validates :author, presence: true
-    validates :price, presence: true, numericality: true
+    validates :price, presence: true, numericality: {greater_than_or_equal_to: 0, message: "must be a positive value" }
     validates :published, presence: true
 end
--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,19 +2,31 @@
 require 'rails_helper'
 
 RSpec.describe "Creating a book", type: :feature do
-    scenario 'valid inputs' do
+    # scenario 'valid inputs' do
+    #     visit new_book_path
+    #     fill_in "book[title]", with: 'harry potter'
+    #     fill_in "book[author]", with: 'J.K. Rowling'
+    #     fill_in "book[price]", with: '10.99'
+    #     fill_in "book[published]", with: '1997-06-26'
+    #     click_on 'Create Book'
+    #     visit books_path
+    #     expect(page).to have_content('harry potter')
+    #     expect(page).to have_content('J.K. Rowling')
+    #     expect(page).to have_content('$10.99')
+    #     expect(page).to have_content('1997-06-26')
+    # end
+
+
+    # SUNNY TEST 
+    scenario 'successfully creates a book' do
         visit new_book_path
-        fill_in "book[title]", with: 'harry potter'
-        fill_in "book[author]", with: 'J.K. Rowling'
-        fill_in "book[price]", with: '10.99'
-        fill_in "book[published]", with: '1997-06-26'
+        fill_in 'book[title]', with: 'harry potter'
         click_on 'Create Book'
-        visit books_path
-        expect(page).to have_content('harry potter')
-        expect(page).to have_content('J.K. Rowling')
-        expect(page).to have_content('$10.99')
-        expect(page).to have_content('1997-06-26')
+        
+        expect(page).to have_content('Book was successfully created.') # FLASH NOTICE
+        expect(Book.count).to eq(1)
     end
+
 
     # RAINY TEST - blank title
     scenario 'does not save a book with blank title' do
@@ -22,7 +34,7 @@ RSpec.describe "Creating a book", type: :feature do
         fill_in 'book[title]', with: ''
         click_on 'Create Book'
         
-        expect(page).to have_content("Title can't be blank") 
+        expect(page).to have_content("Title can't be blank") # FLASH NOTICE
         expect(Book.count).to eq(0)
     end
 end
