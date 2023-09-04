307f5906e73263b42f40b534866f7646414a29da
nathanhtkd <nathanhartojo@gmail.com>
2023-09-04 23:01:28 +0000

    TDD complete

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,39 +2,18 @@
 require 'rails_helper'
 
 RSpec.describe "Creating a book", type: :feature do
-    # scenario 'valid inputs' do
-    #     visit new_book_path
-    #     fill_in "book[title]", with: 'harry potter'
-    #     fill_in "book[author]", with: 'J.K. Rowling'
-    #     fill_in "book[price]", with: '10.99'
-    #     fill_in "book[published]", with: '1997-06-26'
-    #     click_on 'Create Book'
-    #     visit books_path
-    #     expect(page).to have_content('harry potter')
-    #     expect(page).to have_content('J.K. Rowling')
-    #     expect(page).to have_content('$10.99')
-    #     expect(page).to have_content('1997-06-26')
-    # end
-
-
-    # SUNNY TEST 
-    scenario 'successfully creates a book' do
+    scenario 'valid inputs' do
         visit new_book_path
-        fill_in 'book[title]', with: 'harry potter'
+        fill_in "book[title]", with: 'harry potter'
+        fill_in "book[author]", with: 'J.K. Rowling'
+        fill_in "book[price]", with: '10.99'
+        fill_in "book[published]", with: '1997-06-26'
         click_on 'Create Book'
-        
-        expect(page).to have_content('Book was successfully created.') # FLASH NOTICE
-        expect(Book.count).to eq(1)
-    end
+        visit books_path
+        expect(page).to have_content('harry potter')
+        expect(page).to have_content('J.K. Rowling')
+        expect(page).to have_content('$10.99')
+        expect(page).to have_content('1997-06-26')
 
-
-    # RAINY TEST - blank title
-    scenario 'does not save a book with blank title' do
-        visit new_book_path
-        fill_in 'book[title]', with: ''
-        click_on 'Create Book'
-        
-        expect(page).to have_content("Title can't be blank") # FLASH NOTICE
-        expect(Book.count).to eq(0)
     end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -1,36 +1,37 @@
-# location: spec/models/book_spec.rb
+# location: spec/unit/unit_spec.rb
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  subject { described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: 10.99, published: '2001-01-01') }
+    subject do 
+        described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: 10.99, published: '1997-06-26')
+    end
 
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
+    it 'is valid with valid attributes' do
+        expect(subject).to be_valid
+    end
 
-  it 'is not valid without a title' do
-    subject.title = nil
-    expect(subject).not_to be_valid
-  end
+    it 'is not valid without a title' do
+        subject.title = nil
+        expect(subject).not_to be_valid
+    end
 
-  it 'is not valid without an author' do
-    subject.author = nil
-    expect(subject).not_to be_valid
-  end
+    it 'is not valid without an author' do
+        subject.author = nil
+        expect(subject).not_to be_valid
+    end
 
-  it 'is not valid without a price' do
-    subject.price = nil
-    expect(subject).not_to be_valid
-  end
+    it 'is not valid without a price' do
+        subject.price = nil
+        expect(subject).not_to be_valid
+    end
 
-  it 'is not valid with a negative price' do
-    subject.price = -5
-    expect(subject).not_to be_valid
-  end
+    it 'is not valid with non-numeric price' do
+        subject.price = 'ten'
+        expect(subject).not_to be_valid
+    end
 
-  it 'is not valid without a published date' do
-    subject.published = nil
-    expect(subject).not_to be_valid
-  end
-
+    it 'is not valid without a published date' do
+        subject.published = nil
+        expect(subject).not_to be_valid
+    end
 end
