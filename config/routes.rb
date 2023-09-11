6cb1a83c78f13f0793d5c4f572a909cd10427b43
nathanhtkd <nathanhartojo@gmail.com>
2023-09-11 15:07:27 -0500

    add Procfile and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   root 'books#index'
-
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_034002) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "published"
-    t.decimal "price"
     t.string "author"
+    t.decimal "price"
+    t.date "published"
   end
 
 end
