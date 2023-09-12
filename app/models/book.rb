5892cffc4797bcef2cb752bc40b93b1a9a3afdcd
nathanhtkd <nathanhartojo@gmail.com>
2023-09-12 13:36:53 +0000

    added user and user_book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
-    validates :title, presence: true
-    validates :author, presence: true
-    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0, message: "must be a positive value!" }
-    validates :published, presence: true
+    has_many :users, through: :user_books
+    has_many :user_books
+
+    validates :title, :author, :price, :published, presence: true
 end
